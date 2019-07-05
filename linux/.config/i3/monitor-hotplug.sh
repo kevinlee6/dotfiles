34066b27e07c63f51a4c8bdcaa3716f808df644d
Kevin Lee <kevin.lee@listenfirstmedia.com>
2019-07-05 12:18:28 -0400

    Use autorandr to handle monitor switching.

--- linux/.config/i3/monitor-hotplug.sh
+++ linux/.config/i3/monitor-hotplug.sh
@@ -5,9 +5,10 @@ set -e
 
 HDMI_STATUS=$(</sys/class/drm/card0/card0-HDMI-A-1/status )  
 
-if [ "connected" == "$HDMI_STATUS" ]; then  
-    /usr/bin/xrandr --output HDMI1 --display 3440x1440 --above eDP1 --auto
+if [ "$HDMI_STATUS" == 'connected' ]; then  
+    /usr/bin/xrandr --output HDMI1 --mode 3440x1440
+    /usr/bin/xrandr --output eDP1 --off
 else  
     /usr/bin/xrandr --output HDMI1 --off
-    exit
+    /usr/bin/xrandr --output eDP1 --auto
 fi
