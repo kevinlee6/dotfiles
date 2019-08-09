d1a5131e06ec995fc037dc860c3ad15895e259b8
Kevin Lee <kevin.lee@listenfirstmedia.com>
2019-08-09 00:19:17 -0400

    Work on apt-get list. WIP.

---
+++ linux/scripts/apt-get.sh
@@ -0,0 +1,32 @@
+ppa=(
+  neovim-ppa/stable
+)
+
+# Add all ppas with this format:
+sudo add-apt-repository ppa:
+
+packages=(
+  alacritty
+  bash-completion
+  bat
+  fd
+  flameshot
+  fonts-font-awesome
+  fonts-firacode
+  fonts-noto
+  i3
+  i3blocks
+  imagemagick
+  keepassxc
+  neovim
+  network-manager
+  ranger
+  ripgrep
+  rofi
+  trash-cli
+  tree
+  udiskie
+  yad
+)
+
+# pgcli, nvm, rbenv, postgresql
