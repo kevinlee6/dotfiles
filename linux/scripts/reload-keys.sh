bb1d5b92da5c4d0c487be16bcc1b387bc030b4b8
Kevin Lee <kevin.lee@listenfirstmedia.com>
2020-04-23 13:49:24 -0400

    Turns out ! -z and -n might not be equivalent.
    with -n, firefox would cause a failure in this script from set -e.
    -o pipefail is also bash, not sh.

--- linux/scripts/reload-keys.sh
+++ linux/scripts/reload-keys.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eo pipefail
+
 # Ideally used with udev
 
 pid=$(pgrep xcape | xargs)
 
-if [ -n "$pid" ]; then
+if [ ! -z "$pid" ]; then
   # Word splitting wanted; don't double quote
   kill -9 $pid
 fi
