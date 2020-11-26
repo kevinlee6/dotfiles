c3688ad095ce97b8ca51c1b7245b4a5e1bfbb8a1
Kevin Lee <kevin.lee@listenfirstmedia.com>
2020-11-25 23:04:59 -0500

    Move package managers to bash_profile. Add cargo.
    More visible that way, and loads before anything else (modifying PATH).

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -1,4 +1,25 @@
 #!/bin/bash
 
+# Package Managers START =======================================================
+if [ -d "$HOME/.rbenv" ]; then
+  export PATH=$HOME/.rbenv/bin:$PATH
+  eval "$(rbenv init -)"
+fi
+
+if [ -d "$HOME/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
+
+if [ -d "$HOME/.cargo" ]; then
+  export PATH="$HOME/.cargo/bin:$PATH"
+fi
+
+export NVM_DIR="$([ -z ${XDG_CONFIG_HOME-} ] && printf %s ${HOME}/.nvm || printf %s ${XDG_CONFIG_HOME}/nvm)"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# Package Managers END =========================================================
+
 [ -r ~/.bashrc ] && source ~/.bashrc
 [ -r ~/.bashrcs/kevin.bash ] && source ~/.bashrcs/kevin.bash
--- bash/.bashrcs/kevin.bash
+++ bash/.bashrcs/kevin.bash
@@ -57,21 +57,5 @@ if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
 fi
 # Linux END ====================================================================
 
-# Package Managers START =======================================================
-if [ -d "$HOME/.rbenv" ]; then
-  export PATH=$HOME/.rbenv/bin:$PATH
-  eval "$(rbenv init -)"
-fi
-if [ -d "$HOME/.pyenv" ]; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-fi
-
-export NVM_DIR="$([ -z ${XDG_CONFIG_HOME-} ] && printf %s ${HOME}/.nvm || printf %s ${XDG_CONFIG_HOME}/nvm)"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-# Package Managers END =========================================================
-
 [ -r ~/.bashrcs/aliases.bash ] && source ~/.bashrcs/aliases.bash
 [ -r ~/.fzf.bash ] && [ -r ~/.bashrcs/fzf.bash ] && source ~/.bashrcs/fzf.bash
